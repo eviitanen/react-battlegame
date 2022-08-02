0658b76dcd972556188fdae67305c7cfb3c6d05b
etv <erno.viitanen@gmail.com>
2022-08-02 20:15:26 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [comment, setComment] = useState('')
+
+  if (comment === '')
+  setComment('comment')
   return (
     <div className="App">
         <p>
